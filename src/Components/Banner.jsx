47eb87e006578adf6ddf3477f7867387488c7f3d
Sazid60 <shahnawazsazid60@gmail.com>
2024-03-25 22:33:26 +0600

    Banner Added

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,13 +1,14 @@
 
 const Banner = () => {
     return (
-        <div className="hero bg-base-200 mt-14">
+        <div className="hero bg-base-200 mt-6 lg:mt-8 mb-6 rounded-xl">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                <img src="../../public/Banner_Img.png" className="max-w-sm rounded-lg" />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <h1 className="text-3xl lg:text-5xl font-bold max-w-[526px] mb-4 lg:mb-12 text-center lg:text-start">Books to freshen up your bookshelf</h1>
+                    <div className="flex justify-center lg:justify-start">
+                        <button className="btn bg-[#23BE0A] text-white">View The List</button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,25 +2,25 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <div tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 flex gap-4">
-                        <NavLink to={"/"} className={({isActive})=> isActive? 'btn bg-[#23BE0A] text-white' : 'btn bg-white border-0'} >Home</NavLink>
-                        <NavLink to={"/listed-books"} className={({isActive})=> isActive? 'btn bg-[#23BE0A] text-white' : 'btn bg-white border-0'}>Listed Books</NavLink>
-                        <NavLink to={"/pages-to-read"} className={({isActive})=> isActive? 'btn bg-[#23BE0A] text-white' : 'btn bg-white border-0'}>Pages To Read</NavLink>
+                        <NavLink to={"/"} className={({ isActive }) => isActive ? 'btn bg-[#23BE0A] text-white' : 'btn btn-ghost bg-white border-0'} >Home</NavLink>
+                        <NavLink to={"/listed-books"} className={({ isActive }) => isActive ? 'btn bg-[#23BE0A] text-white' : 'btn btn-ghost bg-white border-0'}>Listed Books</NavLink>
+                        <NavLink to={"/pages-to-read"} className={({ isActive }) => isActive ? 'btn bg-[#23BE0A] text-white' : 'btn btn-ghost bg-white border-0'}>Pages To Read</NavLink>
                     </div>
                 </div>
-                <NavLink to={"/"} className="btn btn-ghost text-3xl font-bold">Book Vibe</NavLink>
+                <NavLink to={"/"} className="btn btn-ghost text-lg lg:text-3xl font-bold">Book Vibe</NavLink>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <div className="menu menu-horizontal px-1 flex gap-2">
-                    <NavLink to={"/"} className={({isActive})=> isActive? 'btn bg-[#23BE0A] text-white' : 'btn bg-white border-0'}>Home</NavLink>
-                    <NavLink to={"/listed-books"} className={({isActive})=> isActive? 'btn bg-[#23BE0A] text-white' : 'btn bg-white border-0'}>Listed Books</NavLink>
-                    <NavLink to={"/pages-to-read"} className={({isActive})=> isActive? 'btn bg-[#23BE0A] text-white' : 'btn bg-white border-0'}>Pages To Read</NavLink>
+                    <NavLink to={"/"} className={({ isActive }) => isActive ? 'btn bg-[#23BE0A] text-white' : 'btn btn-ghost bg-white border-0'}>Home</NavLink>
+                    <NavLink to={"/listed-books"} className={({ isActive }) => isActive ? 'btn bg-[#23BE0A] text-white' : 'btn btn-ghost bg-white border-0'}>Listed Books</NavLink>
+                    <NavLink to={"/pages-to-read"} className={({ isActive }) => isActive ? 'btn bg-[#23BE0A] text-white' : 'btn btn-ghost bg-white border-0'}>Pages To Read</NavLink>
                 </div>
             </div>
             <div className="navbar-end flex gap-2 ">
