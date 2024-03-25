bf7c52930e855708673656c9ded064646f6afd68
Sazid60 <shahnawazsazid60@gmail.com>
2024-03-25 22:50:28 +0600

    Resposive Banner Added

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,13 +1,15 @@
 
 const Banner = () => {
     return (
-        <div className="hero bg-base-200 mt-6 lg:mt-8 mb-6 rounded-xl">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="../../public/Banner_Img.png" className="max-w-sm rounded-lg" />
-                <div>
-                    <h1 className="text-3xl lg:text-5xl font-bold max-w-[526px] mb-4 lg:mb-12 text-center lg:text-start">Books to freshen up your bookshelf</h1>
-                    <div className="flex justify-center lg:justify-start">
-                        <button className="btn bg-[#23BE0A] text-white">View The List</button>
+        <div className="px-6 lg:px-0">
+            <div className="bg-base-200 mt-6 lg:mt-8 mb-6 rounded-xl lg:p-8">
+                <div className="flex flex-col lg:flex-row-reverse justify-center items-center">
+                    <img src="../../public/Banner_Img.png" className="max-w-xs rounded-lg m-2" />
+                    <div className="">
+                        <h1 className="text-3xl lg:text-5xl font-bold max-w-[300px] lg:max-w-[526px] mb-4 lg:mb-12 text-center lg:text-start">Books To Freshen Up Your Bookshelf</h1>
+                        <div className="flex justify-center lg:justify-start">
+                            <button className="btn bg-[#23BE0A] text-white">View The List</button>
+                        </div>
                     </div>
                 </div>
             </div>
