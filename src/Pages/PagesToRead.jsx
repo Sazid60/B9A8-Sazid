b29473e8c9470aa8877af1e5d021f65c12d8f6e9
Sazid60 <shahnawazsazid60@gmail.com>
2024-03-27 06:02:03 +0600

    Custom Bar Chart Added

--- src/Pages/PagesToRead.jsx
+++ src/Pages/PagesToRead.jsx
@@ -1,8 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
+import { getReadList } from '../Utilities/index2';
+
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+    ${x + width / 2}, ${y}
+    C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+    Z`;
+    };
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
 
 const PagesToRead = () => {
+    const [chartData, setChartData] = useState([]);
+
+    useEffect(() => {
+        const readList = getReadList()
+        const transformedData = readList.map((item) => ({
+            name: item.bookName,
+            totalPages: item.totalPages
+        }))
+        setChartData(transformedData);
+    }, []);
+
     return (
-        <div>
-            
+        <div className='mt-8'>
+            <ResponsiveContainer width="100%" height={300}>
+                <BarChart
+                    data={chartData}
+                    margin={{
+                        top: 20,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                        {chartData.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                        ))}
+                    </Bar>
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
